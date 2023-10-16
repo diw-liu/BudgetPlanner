132e043fedc5d73a0479da13743e5126cc4f7b27
DiwenLiu <liud7879@gmail.com>
2023-10-16 00:11:18 -0400

    redesign header (no rerender need)

--- frontend/Navigation.tsx
+++ frontend/Navigation.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AntDesign, Ionicons } from '@expo/vector-icons'; 
 import { Auth } from '@aws-amplify/auth';
 import { API } from 'aws-amplify';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import Header from './supplement/header';
 import MonthContext from './supplement/contexts';
 import Statistic from './pages/statistic';
@@ -17,19 +17,20 @@ const getTimes = `
     getTimes
   }
 `
+
 const Navigation = () => {
-  const [months, setMonths] = useState([])
+  
   const [month, setMonth] = useState("")
-  const [user, setUser] = useState({})
+  const [months, setMonths] = useState([])
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   useEffect(() => {
     fetchUser();
+    fetchTimes();
   }, []);
 
   const fetchUser = async () => {
     try {
-      AsyncStorage.clear();
       const jsonValue = await AsyncStorage.getItem('user')
       console.log("jsonvalue"+ jsonValue);
       if(!jsonValue){
@@ -43,38 +44,46 @@ const Navigation = () => {
                 userId: idToken.payload['sub'],
                 accessToken: accessToken.getJwtToken(),
               };
-              setUser(user);
-              setIsSignedIn(true);
               await AsyncStorage.multiSet([['user', JSON.stringify(user)],
-                                           ['userExpire', JSON.stringify(getNextDay())],
-                                           ['accessToken', user.accessToken]]);
-              console.log("user end");
-              const monthValue = await AsyncStorage.getItem('months')
-              // console.log("month" + JSON.parse(monthValue || ""));
-              if(!monthValue){
-                fetchTimes(user.accessToken);
-              }
+                                            ['userExpire', JSON.stringify(getNextDay())],
+                                            ['accessToken', user.accessToken]]);
+              // setUser(user);
+              console.log("user end");   
             })
-      }  
+      }
+      setIsSignedIn(true);
     } catch (err) {
-      console.log("Login initialization error " + err)
+      console.log("Login error " + err)
     }
   }
   
-  const fetchTimes = async (accessToken: string) => {
+  const fetchTimes = async () => {
     try {
-        console.log("begin fetchTimes")
-        const timeData : any = await API.graphql({
-            query: getTimes, 
-            authMode: "AMAZON_COGNITO_USER_POOLS",
-            authToken: accessToken
-        })       
-        setMonths(timeData.data?.getTimes)
-        setMonth(timeData.data?.getTimes.at(-1))
-        await AsyncStorage.setItem('months',  JSON.stringify(timeData.data?.getTimes));
-        console.log("after fetchTimes")
+        const value = await AsyncStorage.getItem('months')
+        console.log("month come on" + value);
+        if(!value){
+            console.log("begin fetchTimes")
+            const accessToken = await AsyncStorage.getItem('accessToken') || ""
+            const timeData : any = await API.graphql({
+                query: getTimes, 
+                authMode: "AMAZON_COGNITO_USER_POOLS",
+                authToken: accessToken
+            })
+            console.log("time data"+timeData.data?.getTimes);
+            await AsyncStorage.setItem('months',  JSON.stringify(timeData.data?.getTimes));
+            setMonths(timeData.data?.getTimes)
+            setMonth(timeData.data?.getTimes.at(-1))
+            console.log("after fetchTimes")
+        } 
+        else {
+            console.log("getting cache");
+            const valueArr = JSON.parse(value);
+            console.log("valueArr"+valueArr)
+            setMonths(valueArr)
+            setMonth(valueArr.at(-1))
+        }
     } catch (err) {
-        console.log('fetchTimes failed', err);
+        console.log('FetchTimes error', err);
     }
   }
 
--- frontend/supplement/header.tsx
+++ frontend/supplement/header.tsx
@@ -1,61 +1,97 @@
-import { useContext, useState } from 'react';
-import { StyleSheet, ScrollView, TouchableOpacity, View } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, TouchableOpacity, View, Button } from 'react-native';
 import { Modal, Portal, Text, Menu, Divider } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import { API } from 'aws-amplify';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { Picker } from '@react-native-picker/picker';
 import MonthContext from './contexts';
 
-const Header = (props:any) => {
+// const getTimes = `
+//   query getTimes{
+//     getTimes
+//   }
+// `
 
+const Header = (props:any) => {
     const months: string[] = props.months
     const { month, setMonth } = useContext(MonthContext);
-    // console.log(months);
+    const [select, setSelect] = useState(month);
 
     const [visible, setVisible] = useState(false);
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
 
-
     // useEffect(() => {
     //     fetchTimes()
-    // }, [props.user])
+    // }, [props.isSignedIn])
 
-   
+    // const fetchTimes = async () => {
+    //     try {
+    //         const value = await AsyncStorage.getItem('months')
+    //         console.log("month come on" + value);
+    //         if(!value){
+    //             console.log("begin fetchTimes")
+    //             const accessToken = await AsyncStorage.getItem('accessToken') || ""
+    //             const timeData : any = await API.graphql({
+    //                 query: getTimes, 
+    //                 authMode: "AMAZON_COGNITO_USER_POOLS",
+    //                 authToken: accessToken
+    //             })
+    //             console.log("time data"+timeData.data?.getTimes);
+    //             await AsyncStorage.setItem('months',  JSON.stringify(timeData.data?.getTimes));
+    //             setMonths(timeData.data?.getTimes)
+    //             setMonth(timeData.data?.getTimes.at(-1))
+    //             console.log("after fetchTimes")
+    //         } 
+    //         else {
+    //             console.log("getting cache");
+    //             const valueArr = JSON.parse(value);
+    //             setMonths(valueArr)
+    //             setMonth(valueArr.at(-1))
+    //         }
+    //     } catch (err) {
+    //         console.log('FetchTimes error', err);
+    //     }
+    // }
 
-    const handleSetSelectMonth = (i: number) =>{
-        setMonth(months[i]);
-        setVisible(false);
+    const handleSetSelectMonth = () =>{
+        setMonth(select);
+        hideModal();
     }
 
+    console.log(months);
+
     return (
-        <SafeAreaView>
-            <StatusBar style="dark"/>
-            <View style={styles.header}>
-                <Portal>
-                    <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.contentContainer}>
-                        <Text style={styles.text}>Select Target Month</Text>
-                        <Divider />
-                        <ScrollView style={styles.scrollView}>
-                            {
-                                months && months.map((month,index) =>(
-                                    <Menu.Item onPress={() => handleSetSelectMonth(index)} title={month} key={index}/>
-                                ))
-                            }
-                        </ScrollView>
-                    </Modal>
-                </Portal>
-                <TouchableOpacity onPress={showModal}>
-                    {
-                        month && <Text>
-                                    {month}
-                                </Text>
-                    }   
-                </TouchableOpacity>
-            </View>
-            {props.children}
-        </SafeAreaView>
+        <>
+            <SafeAreaView >
+                <StatusBar style="dark"/>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={showModal}>
+                        { month && <Text> {month} </Text>}   
+                    </TouchableOpacity>
+                </View>
+                {props.children}
+            </SafeAreaView>
+            {   visible && <View style={styles.container}>
+                                <Picker
+                                    style={styles.pickerStyles}
+                                    selectedValue={select}
+                                    onValueChange={(month) => {
+                                        setSelect(month);
+                                    }}
+                                    enabled={false}
+                                    mode={'dropdown'}>
+                                    {
+                                        months.map(month=> <Picker.Item key={month} label={month} value={month}/>)
+                                    }
+                                </Picker>
+                                <Button title="Confirmed" onPress={handleSetSelectMonth}/>
+                            </View>
+            }
+        </>
+       
     )
 }
 
@@ -87,6 +123,19 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
         height: 44,
     },
+    
+        container: {
+          flex: 1,
+          backgroundColor: 'white',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        pickerStyles:{
+          width:'70%',
+          backgroundColor:'white',
+          color:'white'
+        }
+
 });
 
 export default Header;
